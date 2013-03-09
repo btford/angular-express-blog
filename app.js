ac63ea447465c0697b50ee63980f6e94280ade69
vagnervjs <vagnervjs@gmail.com>
2013-03-09 13:16:40 -0300

    Fix express create server

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
  * Module dependencies.
  */
 
-var express = require('express'),
+var express = require("express"),
   routes = require('./routes'),
   api = require('./routes/api');
 
-var app = module.exports = express.createServer();
+  var app = module.exports = express();
 
 // Configuration
 
@@ -52,5 +52,5 @@ app.get('*', routes.index);
 // Start server
 
 app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", 3000, app.settings.env);
 });
